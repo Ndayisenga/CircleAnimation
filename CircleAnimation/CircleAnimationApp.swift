d549dd30534bbb3c078fe7869d0e0e63d92856ca
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-04 12:07:47 +0200

    Initial Commit

---
+++ CircleAnimation/CircleAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleAnimationApp.swift
+//  CircleAnimation
+//
+//  Created by Ndayisenga Jean Claude on 04/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct CircleAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
