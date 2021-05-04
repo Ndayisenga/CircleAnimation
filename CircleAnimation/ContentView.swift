d8423f1a7640092764615919c07b8b694b2a9c93
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-04 12:45:16 +0200

    Circle Animation

--- CircleAnimation/ContentView.swift
+++ CircleAnimation/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var fill: CGFloat = 0.5
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(UIColor(red: 11/255.0, green: 15/255.0, blue: 128/255.0, alpha: 1))
+            ZStack {
+                // Track circle
+                Circle()
+                    .stroke(Color.white.opacity(0.3),
+                            style: StrokeStyle(lineWidth: 30))
+                // Animation Circle
+                Circle()
+                    .trim(from: 0, to: self.fill)
+                    .stroke(Color.yellow,
+                            style: StrokeStyle(lineWidth: 30))
+                    .rotationEffect(.init(degrees: -90))
+                    .animation(Animation.linear(duration: 0.3))
+                Text("$\(Int(self.fill * 100.00))")
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 52))
+                
+            }
+            .padding(50)
+        }
+        .onTapGesture {
+            for x in 0...100 {
+                DispatchQueue.main.asyncAfter(deadline: .now()+TimeInterval(x/15)) {
+                    self.fill += 0.01
+                }
+            }
+        }
     }
 }
 
